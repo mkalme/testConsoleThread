f66e6f1cc424a6784ab11e4e45bd751d08b5fdbe
mkalme <mikelis.kalme@gmail.com>
2025-05-22 15:22:05 +0300

    Add project files.

---
+++ testConsoleThread/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace testConsoleThread
+{
+    class Program
+    {
+        volatile static int counter = 0;
+        static void Main(string[] args)
+        {
+            List<Task> allTasks = new List<Task>();
+            for (int i = 0; i < 10; i++) {
+                Task t1 = new Task(() => {
+                    count();
+                });
+                allTasks.Add(t1);
+                t1.Start();
+            }
+            Task.WaitAll(allTasks.ToArray());
+
+            Debug.WriteLine(counter);
+
+            Console.Read();
+        }
+
+        public static void count() {
+            for (int i = 0; i < 1000000; i++) {
+                counter++;
+            }
+        }
+    }
+}
